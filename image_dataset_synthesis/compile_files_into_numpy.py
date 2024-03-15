07c35737f737b0aefd209b8760e365a6dbbf02ca
Alex Richardson <william.a.richardson@vanderbilt.edu>
2024-03-15 04:00:40 -0500

    New generator scripts for the diffusion autoencoder data sources.

--- image_dataset_synthesis/compile_files_into_numpy.py
+++ image_dataset_synthesis/compile_files_into_numpy.py
@@ -7,24 +7,7 @@ import sys
 import json
 import cv2
 
-name = sys.argv[1]
-output_image_memmap = name + "_images.npy" #"train.npy"
-output_label_memmap = name + "_labels.npy"
-folder = sys.argv[2]
-json_file = sys.argv[3]
-slice_size = 1000
-jobs = 20
-paths = [e for e in os.listdir(folder) if "jpg" in e]
-image_count = len(paths)
-image_slices = [slice(start, start + slice_size) for start in range(0, image_count - slice_size, slice_size)]
-print(name, output_image_memmap, output_label_memmap, folder, json_file)
-
-json_raw_data = json.load(open(json_file))
-json_data = {}
-for entry in json_raw_data:
-    json_data[entry['name']] = entry['attributes']
-
-del json_raw_data
+# Compiles bdd100k source images into image and numpy files
 
 # Generate our 3 entry numpy array that respectively creates:
 # Daytime (1) or nighttime (0)
@@ -37,7 +20,7 @@ def generate_attribute_data(json_entry):
     result = numpy.array([daytime, highway, clouds], dtype=numpy.uint8)
     return result
 
-def exportToNumpy(memmap_array_image, memmap_array_labels, our_paths, sl):
+def exportToNumpy(folder, json_data, memmap_array_image, memmap_array_labels, our_paths, sl):
     start = sl.start
     stop = sl.stop
     print("Job launched!")
@@ -49,6 +32,7 @@ def exportToNumpy(memmap_array_image, memmap_array_labels, our_paths, sl):
         image_downscaled = cv2.resize(image, dsize=(256,256), interpolation=cv2.INTER_CUBIC)
         json_entry = json_data[image_path] if (image_path in json_data) else {'timeofday': 'not_daytime', 'scene': 'not_highway', 'weather': 'notclear'}
         if ((image_path not in json_data)):
+            print(folder, image_path)
             print("WTF?!")
         memmap_array_image[i] = numpy.array(image_downscaled, dtype=numpy.uint8).transpose((2,0,1))
         memmap_array_labels[i] = generate_attribute_data(json_entry)
@@ -56,11 +40,36 @@ def exportToNumpy(memmap_array_image, memmap_array_labels, our_paths, sl):
         #print(i, i - start)
         sys.stdout.flush()
 
-def runItAll():
+def run(name, folder, json_file):
     print("Launching full run!")
+    output_image_memmap = name + "_images.npy"
+    output_label_memmap = name + "_labels.npy"
+    slice_size = 1000
+    jobs = 20
+    paths = [e for e in os.listdir(folder) if "jpg" in e]
+    image_count = len(paths)
+    image_slices = [slice(start, start + slice_size) for start in range(0, image_count - slice_size, slice_size)]
+    print(name, output_image_memmap, output_label_memmap, folder, json_file)
+
+    json_raw_data = json.load(open(json_file))
+    json_data = {}
+    for entry in json_raw_data:
+        json_data[entry['name']] = entry['attributes']
+
+    del json_raw_data
     output_image = numpy.lib.format.open_memmap(output_image_memmap, dtype=numpy.uint8, shape=(image_count, 3, 256, 256), mode='w+')
     output_label = numpy.lib.format.open_memmap(output_label_memmap, dtype=numpy.uint8, shape=(image_count, 3), mode='w+')
-    joblib.Parallel(n_jobs=jobs)(joblib.delayed(exportToNumpy)(output_image, output_label, paths[sl], sl)
+    joblib.Parallel(n_jobs=jobs)(joblib.delayed(exportToNumpy)(folder, json_data, output_image, output_label, paths[sl], sl)
                    for sl in image_slices)
+
+def runItAll():
+    root_folder = os.path.join(os.path.dirname(__file__), "..")
+    bdd100k_folder = os.path.join(root_folder, "bdd100k")
+    bdd100k_images_train_folder = os.path.join(bdd100k_folder, "images", "100k", "train")
+    bdd100k_images_val_folder = os.path.join(bdd100k_folder, "images", "100k", "val")
+    bdd100k_images_train_labels = os.path.join(bdd100k_folder, "labels", "det_20", "det_train.json")
+    bdd100k_images_val_labels = os.path.join(bdd100k_folder, "labels", "det_20", "det_val.json")
+    run("train", bdd100k_images_train_folder, bdd100k_images_train_labels)
+    run("val", bdd100k_images_val_folder, bdd100k_images_val_labels)
 
 runItAll()
--- image_dataset_synthesis/create_latent_dataset.py
+++ image_dataset_synthesis/create_latent_dataset.py
@@ -1,41 +1,47 @@
 import torch
-from data import OriginalImageDataset as ImageDataset
 import pytorch_lightning as pl
-import first_stage_autoencoder
 from torch.utils.data import DataLoader
 import glob
 from pytorch_lightning.cli import LightningCLI
 import yaml
 import numpy
+import os
+import sys
 
-# Training hyperparameters
-dataset_choice = "Testing"
-base_dir = "/work/cseos2g/papapalpi/"
-dataset_path_train = base_dir + "data/train_float_256x256.npy"
-dataset_path_val = base_dir + "data/val_float_256x256.npy"
-new_dataset_path_train =  base_dir + "data/train_float_256x256_64x64_latent_better.npy"
-new_dataset_path_val =  base_dir + "data/val_float_256x256_64x64_latent_better.npy"
-max_epoch = 10
-batch_size = 32
-device = 'cuda'
-# Create datasets and data loaders
-train_dataset = ImageDataset(dataset_path_train)
-val_dataset = ImageDataset(dataset_path_val)
+if __name__ == '__main__':
 
-autoencoder_model = first_stage_autoencoder.generate_pretrained_model()
-autoencoder_model = autoencoder_model.eval().to(device)
+    def add_root_to_path():
+        sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
-def convertData(dataset, new_path):
-    loader = DataLoader(dataset, batch_size=batch_size, num_workers=4, shuffle=False)
-    output_memmap = numpy.lib.format.open_memmap(new_path, dtype=float, shape=(len(dataset), 6, 64, 64), mode='w+')
-    with torch.no_grad():
-        for i, data in enumerate(loader, 0):
-            data = data.to(device)
-            result = autoencoder_model.encode_raw(data).to('cpu')
-            output_memmap[(i * batch_size):((i * batch_size) + data.shape[0])] = result.numpy()
-            print(i)
+    add_root_to_path()
+    from data import OriginalImageDataset as ImageDataset
+    import first_stage_autoencoder
 
-print("Train")
-convertData(train_dataset, new_dataset_path_train)
-print("Val")
-convertData(val_dataset, new_dataset_path_val)
+    base_dir = os.path.dirname(__file__)
+    dataset_path_train = os.path.join(base_dir, "train_images.npy")
+    dataset_path_val = os.path.join(base_dir, "val_images.npy")
+    new_dataset_path_train = os.path.join(base_dir, "train_images_64x64_latent.npy")
+    new_dataset_path_val = os.path.join(base_dir, "val_images_64x64_latent.npy")
+    batch_size = 32
+    device = 'cuda'
+    # Create datasets and data loaders
+    train_dataset = ImageDataset(dataset_path_train)
+    val_dataset = ImageDataset(dataset_path_val)
+
+    autoencoder_model = first_stage_autoencoder.generate_pretrained_model()
+    autoencoder_model = autoencoder_model.eval().to(device)
+
+    def convertData(dataset, new_path):
+        loader = DataLoader(dataset, batch_size=batch_size, num_workers=4, shuffle=False)
+        output_memmap = numpy.lib.format.open_memmap(new_path, dtype=float, shape=(len(dataset), 6, 64, 64), mode='w+')
+        with torch.no_grad():
+            for i, data in enumerate(loader, 0):
+                data = data.to(device)
+                result = autoencoder_model.encode_raw(data).to('cpu')
+                output_memmap[(i * batch_size):((i * batch_size) + data.shape[0])] = result.numpy()
+                print(i)
+
+    print("Train")
+    convertData(train_dataset, new_dataset_path_train)
+    print("Val")
+    convertData(val_dataset, new_dataset_path_val)
